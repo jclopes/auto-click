258e28f80044b8e62691b5c375ae255144da0c93
João Lopes <jc.lopes@gmail.com>
2020-02-17 02:20:33 +0100

    Big refactor to support more than one button.

--- main.py
+++ main.py
@@ -7,16 +7,29 @@ import numpy as np
 import pyautogui
 
 
-def findButton(img, template):
+def midPoint(template, px, py):
+  is_retina = True
+  xx = px + (len(template[0]) / 2)
+  yy = py + (len(template) / 2)
+  # retina display have double pixels density
+  if(is_retina):
+    xx /= 2
+    yy /= 2
+  return (xx, yy)
+
+
+def findButton(img, templates):
   img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-  w, h = template.shape[::-1]
+  for template in templates:
+    w, h = template.shape[::-1]
 
-  res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-  min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-  if max_val < 0.8:
-    return None
-  return max_loc
+    if max_val > 0.8:
+      px, py = max_loc
+      return midPoint(template, px, py)
+  return None
 
 
 def takeShot():
@@ -35,27 +48,17 @@ def terminate():
 
 
 def main():
-  is_retina = True
-  template = cv.imread('template.png', 0)
-  buttonPos = None
+  templates = list()
+  templates.append(cv.imread('template01.png', 0))
+  templates.append(cv.imread('template02.png', 0))
 
   while True:
-    while (buttonPos == None):
+    match_t = None
+    while (match_t is None):
       time.sleep(1.5)
       screenshot = takeShot()
-      buttonPos = findButton(screenshot, template)
-
-    xx, yy = buttonPos
-    buttonPos = None
-
-    # click on the midle of the template
-    xx += len(template[0]) / 2
-    yy += len(template) / 2
-
-    # retina display have double pixels density
-    if is_retina:
-      xx /= 2
-      yy /= 2
+      match_t = findButton(screenshot, templates)
+    xx, yy = match_t
     pyautogui.click(xx,yy)
 
 

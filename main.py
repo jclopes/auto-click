702c4e5c1796df2a15b739a9962f6d838c4de474
joao <jc.lopes@gmail.com>
2021-01-31 11:27:38 +0000

    Scale down the image to impove performance
     * avoid extra convertion to grayscale.

--- main.py
+++ main.py
@@ -8,33 +8,40 @@ import pyautogui
 
 
 def midPoint(template, px, py):
-  is_retina = True
   xx = px + (len(template[0]) / 2)
   yy = py + (len(template) / 2)
-  # retina display have double pixels density
-  if(is_retina):
-    xx /= 2
-    yy /= 2
   return (xx, yy)
 
+def scalePoint(point, factor):
+  x, y = point
+  return (int(x*factor), int(y*factor))
 
 def findButton(img, templates):
-  img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  """Matches a template against an image an returns the
+  center point where the image was found.
+  Returns None if there is no match.
+  """
+  # scale down the images by a factor of 4. This will help with performance.
+  img_r = cv.resize(img, (0,0), fx=0.25, fy=0.25)
+  
   for template in templates:
     w, h = template.shape[::-1]
 
-    res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+    res = cv.matchTemplate(img_r, template, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
     if max_val > 0.8:
       px, py = max_loc
-      return midPoint(template, px, py)
+      mp = midPoint(template, px, py)
+      # need to revert the downscaling.
+      return scalePoint(mp, 4)
   return None
 
 
 def takeShot():
+  """Takes a Graysacle screenshot."""
   screenshot = pyautogui.screenshot("capture.png")
-  screenshot = cv.cvtColor(np.array(screenshot), cv.COLOR_RGB2BGR)
+  screenshot = cv.cvtColor(np.array(screenshot), cv.COLOR_BGR2GRAY)
   return screenshot
 
 
@@ -48,9 +55,15 @@ def terminate():
 
 
 def main():
+  is_retina = False
+  for i in sys.argv:
+    if i == 'retina':
+      is_retina = True
+  # TODO: check if the template files exist
   templates = list()
-  templates.append(cv.imread('template01.png', 0))
-  templates.append(cv.imread('template02.png', 0))
+  # scale down the images by a factor of 4. This will help with performance.
+  templates.append(cv.resize(cv.imread('template01.png', 0), (0,0), fx=0.25, fy=0.25))
+  templates.append(cv.resize(cv.imread('template02.png', 0), (0,0), fx=0.25, fy=0.25))
 
   while True:
     match_t = None
@@ -58,8 +71,15 @@ def main():
       time.sleep(1.5)
       screenshot = takeShot()
       match_t = findButton(screenshot, templates)
+
+    if (is_retina):
+      # retina screens have double pixel dencity
+      match_t = scalePoint(match_t, 0.5)
     xx, yy = match_t
+
     pyautogui.click(xx,yy)
+    time.sleep(0.5)
+    pyautogui.moveTo(0, 0)
 
 
 def signal_handler(signal, frame):
